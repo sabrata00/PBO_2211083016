12c0a3816ca1296912ef57872b05ecae16b28e78
sabrata00 <127170484+sabrata00@users.noreply.github.com>
2023-05-27 11:43:20 +0700

    push

---
+++ src/adit/dao/AnggotaDao.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package adit.dao;
+
+import adit.model.Anggota;
+
+/**
+ *
+ * @author LAB-MM
+ */
+public interface AnggotaDao {
+   void insert (Anggota anggota) throws Exception;
+
+
+    
+}
